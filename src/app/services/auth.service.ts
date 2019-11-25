dfd68b45b63718771cd789071775da6b288273d4
Sasathorn Ariyasupapong <sasathorn.ari@gmail.com>
2019-11-25 18:04:00 +0700

    create auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,31 +3,52 @@ import { HttpService } from "./http.service";
 import { StorageService } from "./storage.service";
 import { Router } from "@angular/router";
 import { AuthConstants } from "app/config/auth-constants";
-import { Observable } from "rxjs";
+import { Observable, BehaviorSubject } from "rxjs";
+import { Storage } from '@ionic/storage';
+import { Platform } from '@ionic/angular';
+
+const TOKEN_KEY = 'auth-token';
 
 @Injectable({
   providedIn: "root"
 })
 export class AuthService {
+
+  authenticationState = new BehaviorSubject(false);
+
   constructor(
     private httpService: HttpService,
     private storageService: StorageService,
-    private router: Router
-  ) {}
-
-  login(postData: any): Observable<any> {
-    console.log(postData);
-    return this.httpService.post(`/user`, postData);
-  }
-
-  logout() {
-    this.storageService.removeStorageItem(AuthConstants.AUTH).then(res => {
-      this.router.navigate([`/login`]);
+    private router: Router,
+    private storage: Storage,
+    private plt: Platform
+  ) {
+    this.plt.ready().then(() => {
+      this.chekckToken();
     });
   }
 
-  userlogin(postData: any) {
-    return this.httpService.post(`userlogin`, postData);
+  login(){
+    return this.storage.set(TOKEN_KEY, 'Bearer 123456').then(res => {
+      this.authenticationState.next(true);
+    });
+  }
+
+  logout() {
+    return this.storage.remove(TOKEN_KEY).then(() => {
+      this.authenticationState.next(false);
+    });
+  }
+
+  isAuthenticated(){
+    return this.authenticationState.value;
   }
   
+  chekckToken(){
+    return this.storage.get(TOKEN_KEY).then(res => {
+      if(res){
+        this.authenticationState.next(true);
+      }      
+    });
+  }
 }
