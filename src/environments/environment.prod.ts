f22f0131bac2cc874793f29319ab24a5a2da6f07
sasathornari <sasathorn.ari@gmail.com>
2019-12-23 23:57:40 +0700

    change apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //apiUrl: 'http://portal.gel.co.th:3000/api/'
-  apiUrl: 'http://147.50.42.162:3000/api/'
+  apiUrl: 'http://services.gel.co.th:3000/api/'
+  //apiUrl: 'http://147.50.42.162:3000/api/'
 };
